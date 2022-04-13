aae35b9905881abd7785370bae6e6d03b3410314
Acid Bubbles <acidbubbles.anon@gmail.com>
2022-04-12 23:37:24 -0400

    Import BlendShapeController from utilities

--- BlendShapes.cs
+++ BlendShapes.cs
@@ -1,6 +1,38 @@
+using UnityEngine;
+
 public class BlendShapes : MVRScript
 {
     public override void Init()
     {
+        var renderer = containingAtom.gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
+        if (renderer == null)
+        {
+            SuperController.LogError("The atom '" + containingAtom.name + "' does not have any SkinnedMeshRenderer");
+            return;
+        }
+
+        var mesh = renderer.sharedMesh;
+        if (mesh == null || mesh.blendShapeCount == 0)
+        {
+            SuperController.LogError("The atom '" + containingAtom.name + "' does not have any blend shapes");
+            return;
+        }
+
+        var blendShapeIndex = 0;
+        do
+        {
+            var localBlendShapeIndex = blendShapeIndex;
+            var storable = new JSONStorableFloat(
+                mesh.GetBlendShapeName(blendShapeIndex),
+                renderer.GetBlendShapeWeight(blendShapeIndex),
+                new JSONStorableFloat.SetFloatCallback(val => renderer.SetBlendShapeWeight(localBlendShapeIndex, val)),
+                0f,
+                100f,
+                false
+            );
+            RegisterFloat(storable);
+            CreateSlider(storable);
+        }
+        while (++blendShapeIndex < renderer.sharedMesh.blendShapeCount);
     }
 }
